df5ab56e328ddfd00b2c0abc42a960c46922440c
chetanmoradiya <chetan.moradiya@gmail.com>
2023-06-17 19:11:45 +0530

    first commit

---
+++ src/main/java/com/iris/hackathon/orchestrator/PayloadOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.iris.hackathon.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayloadOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayloadOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iris/hackathon/orchestrator/PayloadOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris.hackathon.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayloadOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
