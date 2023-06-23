28a12d18027d0d95e99042ce0147f76a8cfd81ea
akmkbkk <kkumarr.anand17@gmail.com>
2023-06-24 00:45:01 +0530

    Added Load Sim

--- src/test/resources/db/create_scripts.sql
+++ src/test/resources/db/create_scripts.sql
@@ -4,16 +4,17 @@ drop table if exists payload;
 
 CREATE TABLE if not exists payload(
     payload_id UUID not NULL,
-    file_name VARCHAR (50) NOT NULL,
+    file_name VARCHAR (100) NOT NULL,
+    ftp_folder VARCHAR(30) not null,
     payload_state VARCHAR(20) not null,
     rejection_reason VARCHAR(20),
-    absolute_path VARCHAR (100) NOT NULL,
+    absolute_path VARCHAR (200) NOT NULL,
     create_ts TIMESTAMP NOT NULL,
     update_ts TIMESTAMP,
     last_modified_ts TIMESTAMP not null,
     instruction_count int4,
-    resp_file_name VARCHAR(50),
-    resp_file_path VARCHAR(100),
+    resp_file_name VARCHAR(100),
+    resp_file_path VARCHAR(200),
     constraint payload_pk primary key (payload_id,create_ts)
 );
 
